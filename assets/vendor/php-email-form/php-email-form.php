6a9441e54045ae3f5040d5940f1ed63b967e9820
Viral Patel <40652610+recroviral@users.noreply.github.com>
2023-10-04 00:30:32 -0400

    Update php-email-form.php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,60 +1,38 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Replace with your actual receiving email address
-    $to = 'info@example.com';
-    
-    // Sanitize user inputs to prevent email header injections
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-    
-    // Check if all required fields are filled
-    if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
-        // Compose the email message
-        $headers = "From: $name <$email>\r\n";
-        $headers .= "Reply-To: $email\r\n";
-        $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
-        
-        $emailBody = "Name: $name\n";
-        $emailBody .= "Email: $email\n";
-        $emailBody .= "Subject: $subject\n";
-        $emailBody .= "Message:\n$message";
-        
-        // Send the email
-        if (mail($to, $subject, $emailBody, $headers)) {
-            echo "Success! Your message has been sent.";
-        } else {
-            echo "Error: Unable to send the message. Please try again later.";
+    // Replace with your email address where you want to receive emails
+    $to_email = 'info@cubittax.com';
+
+    try {
+        // Get form data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        // Validate the data (you can add more validation if necessary)
+        if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+            throw new Exception('Please fill out all fields.');
         }
-    } else {
-        echo "Error: All fields are required.";
+
+        // Compose the email message
+        $headers = "From: $email";
+        $message_body = "Name: $name\n";
+        $message_body .= "Email: $email\n";
+        $message_body .= "Subject: $subject\n\n";
+        $message_body .= "Message:\n$message";
+
+        // Send the email
+        if (mail($to_email, $subject, $message_body, $headers)) {
+            echo 'OK';
+        } else {
+            throw new Exception('Error sending email.');
+        }
+    } catch (Exception $e) {
+        echo 'Error: ' . $e->getMessage();
     }
+} else {
+    // Handle non-POST requests (e.g., direct access to this PHP file)
+    echo 'Invalid request.';
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-</head>
-<body>
-    <h1>Contact Us</h1>
-    <form method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="subject">Subject:</label>
-        <input type="text" id="subject" name="subject" required><br><br>
-        
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea><br><br>
-        
-        <input type="submit" value="Send">
-    </form>
-</body>
-</html>
